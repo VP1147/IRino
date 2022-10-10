1ce332b11bbc761efe79b636496c683183c66f6e
Vinícius Pavão <viniciuspavao2004@gmail.com>
2022-10-10 13:03:41 -0400

    Create recorder.ino

---
+++ recorder/recorder.ino
@@ -0,0 +1,40 @@
+int Sensor = A0;
+int t = 1000;             // Reading per second
+int Size = 500;
+int data[500] = {};
+int b_record = 5;
+int b_reproduce = 6;
+int led = 7;
+
+void setup() {
+  //Serial.begin(9600);
+  //pinMode(Sensor, INPUT);
+  pinMode(b_record, INPUT);
+  pinMode(b_reproduce, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  
+  if(digitalRead(b_record) == HIGH) {
+    digitalWrite(led, HIGH);
+    for(int d = 0; d < Size; d++) {
+     data[d] = int(analogRead(Sensor));
+     //Serial.println(data[d]);
+     delay(int(1000/t));
+    }
+  }
+  
+  else if(digitalRead(b_reproduce) == HIGH) {
+    for(int d = 0; d < Size; d++) { 
+      analogWrite(led, int((data[d]-30)*2));
+      //Serial.println(data[d]);
+      delay(1000/t); 
+      }
+  }
+  else {
+    digitalWrite(led, LOW);
+    }
+}
+
+  // for(int p = 0; p <= Size; p++) { Serial.println(data[p]); }
